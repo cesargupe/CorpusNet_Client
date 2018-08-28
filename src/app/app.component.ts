938b4c1f0ae9652080b5798335d735bfd4fd7155
Cesar Gutierrez <cgutip00@estudiantes.unileon.es>
2018-08-28 14:20:49 +0200

    Add inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CorpusNet';
+
+export class AppComponent implements OnInit{
+  public title = 'CorpusNet';
+  public router : Router;
+
+  constructor(private _router: Router ) {
+    this.router = _router;
+  }
+
+  ngOnInit(){
+
+  }
+
 }
